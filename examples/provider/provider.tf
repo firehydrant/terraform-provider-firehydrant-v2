a6305e3bf78a9e1b3ca3eb1971f4b310c4271333
Danielle Leong <2171822+dmleong@users.noreply.github.com>
2024-12-31 12:37:10 -0500

    first commit

---
+++ examples/provider/provider.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    firehydrant-terraform-sdk = {
+      source  = "speakeasy/firehydrant-terraform-sdk"
+      version = "0.0.1"
+    }
+  }
+}
+
+provider "firehydrant-terraform-sdk" {
+  # Configuration options
+}
